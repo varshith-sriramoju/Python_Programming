3160a9ee311a222c6ea137daa0e99d5d82d869d0
Varshith <sriramoju.varshith@outlook.com>
2025-08-31 22:28:14 +0530

    commit

---
+++ OOPS/5.Encapsulation.py
@@ -0,0 +1,30 @@
+#binding of data attributes(variables) and data methods(functions) into single unit
+#restict access form outside the class
+#about private variables
+#__init__()called caonstructor
+#__del__() called destructor
+class Emp:
+    def __init__(self):
+        self.name="varshi"
+        self.age=22
+        self.__salary=45000 #private variables
+    def show(self):
+        print(self.__salary)
+    def __priMethod(self):
+        print()
+    def __str__(self):
+        return f"Age:{self.age}"
+    def __del__(self): #auto executed
+        print("successfully destroyed")
+a=Emp()
+print(a.name,a.age)#no encap as accessing outside the class
+#print(a.__salary) cannot access but can by instance/obj methods
+a.show() #accessed pri vari through ins/obj method
+#a.__priMethod() as pri vari
+#but we can accessed it through _****__
+print(a._Emp__salary) #this way we can access pri variable
+#in pyton ntg is private
+print(dir(a))
+print(a.__str__())
+del a #forcly called destructor but not if obj have more than 1 refer
+print("done")
